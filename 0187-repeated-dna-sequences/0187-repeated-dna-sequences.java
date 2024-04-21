4b65d6095aad9f45e31c19394333b36f8ce9644b
Govindraj Chavan <126868326+govindraj-7c@users.noreply.github.com>
2024-04-21 12:05:08 +0530

    Time: 2 ms (100.00%), Space: 44.8 MB (96.82%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,29 +1,17 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        if(n<=10 || n>10000) return new ArrayList<>();
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> ans = new HashSet<>();
 
-        if(s.length() < 10){
-            return list;
-        }
-
-        for(int i=0; i<10; i++){
-            sb.append(s.charAt(i));
-        }
-        map.put(sb.toString(), 1);
-
-        for(int i=10; i<s.length(); i++){
-            sb.deleteCharAt(0);
-            sb.append(s.charAt(i));
-            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0)+1);
-        }
-
-        for(String key : map.keySet()){
-            if(map.get(key) > 1){
-                list.add(key);
+        for(int i=0; i+9<n; i++){
+            String temp = s.substring(i,i+10);
+            if(!seen.add(temp)){
+                ans.add(temp);
             }
         }
-        return list;
+
+        return new ArrayList<>(ans);
     }
 }
