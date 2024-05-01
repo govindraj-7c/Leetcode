23826703b291174f609fb479e79f30e587b848e7
Govindraj Chavan <126868326+govindraj-7c@users.noreply.github.com>
2024-05-01 21:17:04 +0530

    Time: 1 ms (49.93%), Space: 42 MB (18.43%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int firstOccurence = word.indexOf(ch);
+        if(firstOccurence == -1) return word;
+
+        StringBuilder prefix = new StringBuilder(word.substring(0, firstOccurence+1));
+        return prefix.reverse().toString() + word.substring(firstOccurence+1);
+    }
+}
