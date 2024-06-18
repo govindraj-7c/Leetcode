978d8203ca0dae3695fdbd4baed77884cfdd3ee3
Govindraj Chavan <126868326+govindraj-7c@users.noreply.github.com>
2024-06-18 09:18:11 +0530

    Time: 584 ms (9.82%), Space: 45.6 MB (58.84%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfitAssignment(int[] d, int[] p, int[] w) {
+        int n = p.length;
+        int[][] arr = new int[n][2];
+        int totalProfit = 0;
+
+        for(int i=0; i<d.length; i++){
+            arr[i][0] = d[i];
+            arr[i][1] = p[i];
+        }
+
+        Arrays.sort(arr, (a,b) -> a[0] - b[0]);
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<w.length; i++){
+            int j=0;
+            int maxP = Integer.MIN_VALUE;
+            while(j<n && arr[j][0] <= w[i]){
+                maxP = Math.max(maxP, arr[j][1]);
+                j++;
+            }
+            if(maxP != Integer.MIN_VALUE){
+                totalProfit += maxP;
+            }
+        }
+
+        return totalProfit;
+    }
+}
