bb08aa792170a8899aebc280286231c4386a9010
Govindraj Chavan <126868326+govindraj-7c@users.noreply.github.com>
2024-07-10 11:34:59 +0530

    Time: 1 ms (99.42%), Space: 42.2 MB (26.85%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,24 +1,16 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> stack = new Stack<>();
         int count = 0;
 
         for(String str : logs){
-            if(str.equals("../") && !stack.isEmpty()){
-                stack.pop();
+            if(!str.equals("../") && !str.equals("./")){
+                count++;
             }
-            else if(str.equals("./")){
-                continue;
-            }
-            else if(!str.equals("./") && !str.equals("../")){
-                stack.push(str);
+            else if(str.equals("../") && count != 0){
+                count--;
             }
         }
 
-        while(!stack.isEmpty()){
-            count++;
-            stack.pop();
-        }
         return count;
     }
 }
