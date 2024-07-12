b42e38f421ed40d33c73177d6a1c88ed927924a6
Govindraj Chavan <126868326+govindraj-7c@users.noreply.github.com>
2024-07-12 22:14:04 +0530

    Time: 804 ms (57.67%), Space: 0B (100%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT name, SUM(amount) AS balance FROM Users INNER JOIN Transactions
+ON Users.account = Transactions.account
+GROUP BY Transactions.account
+HAVING SUM(amount) > 10000;
